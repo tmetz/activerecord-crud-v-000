7793b55c0cea8dfcfc30cdf8b318848a2eec82ae
Tammy Metz <tammy@tammymetz.com>
2018-09-30 18:05:57 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(title="Home Alone", release_date=1990)
   # release_date == 1990
 
   Movie.create do |m|
-    movie = movie.new
+    movie = Movie.new
     movie.title = title
     movie.release_date = release_date
     movie.save
