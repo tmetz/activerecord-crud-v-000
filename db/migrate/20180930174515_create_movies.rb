e0b60f5d77bd79128552b558f43949f054279dd2
Tammy Metz <tammy@tammymetz.com>
2018-09-30 17:48:13 +0000

    automatically backed up by learn

--- db/migrate/20180930174515_create_movies.rb
+++ db/migrate/20180930174515_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
   end
 end
